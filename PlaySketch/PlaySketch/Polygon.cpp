fd8f06bd9524a7deb975dbc306d5731b8641d7e1
liuyang <a0077978@gmail.com>
2013-03-06 08:04:41 +0800

    clear files

--- PlaySketch/PlaySketch/Polygon.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Polygon.cpp
-//  PlaySketch
-//
-//  Created by Yang Liu on 31/1/13.
-//  Copyright (c) 2013 Singapore Management University. All rights reserved.
-//
-
-#include "Polygon.h"
